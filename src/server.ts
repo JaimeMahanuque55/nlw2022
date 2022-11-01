00ec1b57a668234dad4cb4cb8ce30835cca0139e
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-11-01 14:15:26 +0200

    testing the table

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrao() {
 
   const fastify = Fastify({
     logger: true,
-  });
+  })
 
-  fastify.get('/pools/count', () => {
-    return { count: 0 }
+  fastify.get('/pools/count', async () => {
+    // const pools = await prisma.pool.findMany({
+    //   where: {
+    //     code: {
+    //       startsWith: 'J'
+    //     }
+    //   }
+    // })
+    const count = await prisma.pool.count()
+
+    return { count }
   })
 
   await fastify.listen({ port: 3333 })
